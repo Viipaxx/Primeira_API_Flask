6d3c3b840e6bb1195cb25bae381a4f5652cbfd13
Vitor Paz <kngatuno@gmail.com>
2024-06-01 16:54:51 -0300

    Novas rotas adicionadas

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def ola():
-    return 'Olá mundo!'
+@app.route('/<int:id>')
+def pessoa(id):
+    return jsonify({'id': id,'nome': 'vitor', 'profissao': 'desenvolvedor'})
 
+@app.route('/soma', methods=['POST', 'PUT', 'GET'])
+def soma():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        total = sum(dados['valores'])
+        return jsonify({'soma': total})
+
+    elif request.method == 'GET':
+        total = 10 + 10
+        return jsonify({'soma': total})
+
+    elif request.method == 'PUT':
+        return 'Sem dados.'
+
+    return 'Método Incorreto'
 
 if __name__ == '__main__':
     app.run(debug=True)
